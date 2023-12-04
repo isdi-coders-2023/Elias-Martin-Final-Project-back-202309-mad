1597a16307ca64ed040370925157d2ce8a13e380
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-04 17:28:08 +0100

    Add clothingItem and user entities and httpError types

---
+++ src/entities/clothingItem.ts
@@ -0,0 +1,15 @@
+import { ImgData } from '../types/img.data.js';
+import { User } from './user.js';
+
+export type ClothingItem = {
+  id: string;
+  name: string;
+  size: string;
+  price: string;
+  clothingItemHeight: string;
+  clothingItemWidth: string;
+  clothingItemFrontImg: ImgData;
+  clothingItemBackImg: ImgData;
+  tares: string;
+  creator: User;
+};
