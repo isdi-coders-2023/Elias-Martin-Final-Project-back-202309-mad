61e4fa6feb4933eabded9d681bb3e1b507cce3c3
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-05 19:33:56 +0100

    Try to fix a coverage Sonar problem

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,11 @@ export default {
   testPathIgnorePatterns: ['dist'],
   resolver: 'jest-ts-webcompat-resolver',
   coveragePathIgnorePatterns: [
-    'src/repos/users/users.mongo.model.ts',
     'src/controllers/controller.ts',
+    'src/repos/users.mongo.model.ts',
+    'src/repos/repo.ts',
     'src/app.ts',
     'src/index.ts',
-    'src/routers/users.routes.ts',
+    'src/routers/users.router.ts',
   ],
 };
