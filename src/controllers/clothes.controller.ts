c24811e2d4e7581bce6e948e11d1c51542be0927
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-09 16:45:27 +0100

    Add clothes route, repo, model and controller and its own route in app

--- src/controllers/clothes.controller.ts
+++ src/controllers/clothes.controller.ts
@@ -1,28 +1,28 @@
-// Temporary import { NextFunction, Request, Response } from 'express';
-// import createDebug from 'debug';
-// import { Repository } from '../repos/repo.js';
-// import { ClothingItem } from '../entities/clothingItem.js';
-// import { Controller } from './controller.js';
-// import { HttpError } from '../types/http.error.js';
+import { NextFunction, Request, Response } from 'express';
+import createDebug from 'debug';
+import { Repository } from '../repos/repo.js';
+import { ClothingItem } from '../entities/clothingItem.js';
+import { Controller } from './controller.js';
+import { HttpError } from '../types/http.error.js';
 
-// const debug = createDebug('EPV:clothes:controller');
+const debug = createDebug('EPV:clothes:controller');
 
-// export class ClothesController extends Controller<ClothingItem> {
-//   constructor(protected repo: Repository<ClothingItem>) {
-//     super(repo);
-//     debug('Instantiated');
-//   }
+export class ClothesController extends Controller<ClothingItem> {
+  constructor(protected repo: Repository<ClothingItem>) {
+    super(repo);
+    debug('Instantiated');
+  }
 
-//   async create(req: Request, res: Response, next: NextFunction) {
-//     try {
-//       req.body.creator = { id: req.body.userId };
-//       if (!req.file)
-//         throw new HttpError(406, 'Not Acceptable', 'Invalid multer file');
-//       const imgData = await this.cloudinaryService.uploadImage(req.file.path);
-//       req.body.clothingItemFrontImg = imgData;
-//       super.create(req, res, next);
-//     } catch (error) {
-//       next(error);
-//     }
-//   }
-// }
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      req.body.creator = { id: req.body.userId };
+      if (!req.file)
+        throw new HttpError(406, 'Not Acceptable', 'Invalid multer file');
+      const imgData = await this.cloudinaryService.uploadImage(req.file.path);
+      req.body.clothingItemFrontImg = imgData;
+      super.create(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
--- src/repos/clothes/clothes.mongo.model.ts
+++ src/repos/clothes/clothes.mongo.model.ts
@@ -1,59 +1,59 @@
-// Temporary import { Schema, model } from 'mongoose';
-// import { ClothingItem } from '../../entities/clothingItem.js';
+import { Schema, model } from 'mongoose';
+import { ClothingItem } from '../../entities/clothingItem.js';
 
-// const clothesSchema = new Schema<ClothingItem>({
-//   name: {
-//     type: String,
-//   },
-//   size: {
-//     type: String,
-//   },
-//   price: {
-//     type: String,
-//   },
-//   clothingItemHeight: {
-//     type: String,
-//   },
-//   clothingItemWidth: {
-//     type: String,
-//   },
-//   clothingItemFrontImg: {
-//     publicId: String,
-//     size: Number,
-//     height: Number,
-//     width: Number,
-//     format: String,
-//     url: String,
-//   },
-//   clothingItemBackImg: {
-//     publicId: String,
-//     size: Number,
-//     height: Number,
-//     width: Number,
-//     format: String,
-//     url: String,
-//   },
-//   tares: {
-//     type: String,
-//   },
-//   creator: {
-//     type: Schema.Types.ObjectId,
-//     ref: 'User',
-//   },
-// });
+const clothesSchema = new Schema<ClothingItem>({
+  name: {
+    type: String,
+  },
+  size: {
+    type: String,
+  },
+  price: {
+    type: String,
+  },
+  clothingItemHeight: {
+    type: String,
+  },
+  clothingItemWidth: {
+    type: String,
+  },
+  clothingItemFrontImg: {
+    publicId: String,
+    size: Number,
+    height: Number,
+    width: Number,
+    format: String,
+    url: String,
+  },
+  clothingItemBackImg: {
+    publicId: String,
+    size: Number,
+    height: Number,
+    width: Number,
+    format: String,
+    url: String,
+  },
+  tares: {
+    type: String,
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
 
-// clothesSchema.set('toJSON', {
-//   transform(_document, returnedObject) {
-//     returnedObject.id = returnedObject._id;
-//     delete returnedObject._id;
-//     delete returnedObject.__v;
-//     delete returnedObject.passwd;
-//   },
-// });
+clothesSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwd;
+  },
+});
 
-// // PUNTO CRÍTICO
-// export const ClothingItemModel = model<ClothingItem>(
-//   'ClothingItem',
-//   clothesSchema,
-//   'clothes'
-// );
+// PUNTO CRÍTICO
+export const ClothingItemModel = model<ClothingItem>(
+  'ClothingItem',
+  clothesSchema,
+  'clothes'
+);
