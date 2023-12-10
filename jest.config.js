e9d64bda9a58fdfc4a565462259fc575af9b5fa2
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-10 15:38:46 +0100

    Add clothes repo test

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,11 @@ export default {
   resolver: 'jest-ts-webcompat-resolver',
   coveragePathIgnorePatterns: [
     'src/repos/users/users.mongo.model.ts',
+    'src/repos/clothes/clothes.mongo.model.ts',
     'src/repos/repo.ts',
     'src/app.ts',
     'src/index.ts',
     'src/routers/users.routes.ts',
+    'src/routers/clothes.routes.ts',
   ],
 };
---
+++ src/repos/clothes/clothes.mongo.repo.test.ts
@@ -0,0 +1,113 @@
+import { ClothesMongoRepo } from './clothes.mongo.repo';
+import { ClothingItemModel } from './clothes.mongo.model';
+import { ClothingItem } from '../../entities/clothingItem';
+import { UsersMongoRepo } from '../users/users.mongo.repo';
+
+jest.mock('./clothes.mongo.model.js');
+
+describe('Given ClothesMongoRepo', () => {
+  let repo: ClothesMongoRepo;
+
+  describe('When we isntantiate it without errors', () => {
+    const exec = jest.fn().mockResolvedValue('Test');
+
+    beforeEach(() => {
+      ClothingItemModel.find = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+
+      ClothingItemModel.findById = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+      ClothingItemModel.findByIdAndUpdate = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+      ClothingItemModel.findByIdAndDelete = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+      ClothingItemModel.create = jest.fn().mockResolvedValue('Test');
+      repo = new ClothesMongoRepo();
+    });
+
+    test('Then it should execute getAll', async () => {
+      const result = await repo.getAll();
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute getById', async () => {
+      const result = await repo.getById('');
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute create', async () => {
+      UsersMongoRepo.prototype.getById = jest
+        .fn()
+        .mockResolvedValue({ clothes: [] });
+      UsersMongoRepo.prototype.update = jest.fn();
+      const result = await repo.create({ author: {} } as Omit<
+        ClothingItem,
+        'id'
+      >);
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute search', async () => {
+      const result = await repo.search({ key: 'size', value: 'M' });
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute update', async () => {
+      const result = await repo.update('', { name: 'Bomber' });
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+    // Test('Then it should execute delete', async () => {
+    //   const result = await repo.delete('');
+    //   expect(exec).toHaveBeenCalled();
+    //   expect(result).toBe('Test');
+    // });
+  });
+
+  describe('When we isntantiate it WITH errors', () => {
+    const exec = jest.fn().mockRejectedValue(new Error('Test'));
+    beforeEach(() => {
+      ClothingItemModel.findById = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+      ClothingItemModel.findByIdAndUpdate = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+      ClothingItemModel.findByIdAndDelete = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+      repo = new ClothesMongoRepo();
+    });
+
+    test('Then getById should throw an error', async () => {
+      expect(repo.getById('')).rejects.toThrow();
+    });
+    test('Then update should throw an error', async () => {
+      expect(repo.update('', { name: 'Bomber' })).rejects.toThrow();
+    });
+    test('Then delete should throw an error', async () => {
+      expect(repo.delete('')).rejects.toThrow();
+    });
+  });
+});
