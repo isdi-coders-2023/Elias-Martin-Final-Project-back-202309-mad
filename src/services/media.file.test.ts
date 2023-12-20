a366ea9c3c3c1ac9ec95f93523bfbea98c9a5293
eliasmr98 <eliasmartinrivas@gmail.com>
2023-12-20 14:06:26 +0100

    Fix media.file test

--- src/services/media.file.test.ts
+++ src/services/media.file.test.ts
@@ -5,14 +5,14 @@ import { HttpError } from '../types/http.error';
 describe('Given the Media File class', () => {
   describe('When we use its methods with a valid imagePath', () => {
     test('Then should upload an image successfully', async () => {
-      const imagePath = 'valid/image/path.jpg';
+      const imagePath = 'valid/image/path.webp';
       const uploadApiResponse = {
-        url: 'https://example.com/image.jpg',
+        url: 'https://example.com/image.webp',
         public_id: 'public_id',
         bytes: 1000,
         height: 500,
         width: 500,
-        format: 'jpg',
+        format: 'webp',
       };
       cloudinary.uploader.upload = jest
         .fn()
@@ -23,14 +23,15 @@ describe('Given the Media File class', () => {
         use_filename: true,
         unique_filename: false,
         overwrite: true,
+        format: 'webp',
       });
       expect(result).toEqual({
-        url: 'https://example.com/image.jpg',
+        url: 'https://example.com/image.webp',
         publicId: 'public_id',
         size: 1000,
         height: 500,
         width: 500,
-        format: 'jpg',
+        format: 'webp',
       });
     });
     describe('When we use its methods with errors', () => {
